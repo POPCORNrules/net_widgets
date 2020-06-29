79a30a829275973ca542840a418d63086f4d2b59
POPCORNrules <tony.arredondo5@gmail.com>
2020-06-29 00:08:59 -0500

    Update wireless.lua

--- wireless.lua
+++ wireless.lua
@@ -5,6 +5,7 @@ local naughty       = require("naughty")
 local gears         = require("gears")
 local cairo         = require("lgi").cairo
 local module_path = (...):match ("(.+/)[^/]+$") or ""
+local theme         = beautiful.get()
 
 function dbg(message)
     naughty.notify({ preset = naughty.config.presets.normal,
